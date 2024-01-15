7aafd44558cbbac62623bc690ca8b7a5ee5d4a96
dassajib <sajibdas.sd@gmail.com>
2024-01-15 23:35:30 +0600

    navbar design done

--- src/Index/Index.jsx
+++ src/Index/Index.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from '../components/Navbar/Navbar'
-import Feed from '../components/Feed/Feed'
-import VideoDetails from '../components/VideoDetails/VideoDetails'
-import ChannelDetails from '../components/ChannelDetails/ChannelDetails'
-import SearchFeed from '../components/SearchFeed/SearchFeed'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Feed from "../components/Feed/Feed";
+import VideoDetails from "../components/VideoDetails/VideoDetails";
+import ChannelDetails from "../components/ChannelDetails/ChannelDetails";
+import SearchFeed from "../components/SearchFeed/SearchFeed";
+import { Box } from "@mui/material";
 
 const Index = () => {
   return (
     <BrowserRouter>
+      <Box sx={{ background: "#000" }}>
         <Navbar />
         <Routes>
-            <Route path='/' exact element={<Feed />} />
-            <Route path='/video/:id' exact element={<VideoDetails />} />
-            <Route path='/channel/:channelId' exact element={<ChannelDetails />} />
-            <Route path='/searchterm' exact element={<SearchFeed />} />
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetails />} />
+          <Route
+            path="/channel/:channelId"
+            exact
+            element={<ChannelDetails />}
+          />
+          <Route path="/search/:searchterm" exact element={<SearchFeed />} />
         </Routes>
+      </Box>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+import { Search } from "@mui/icons-material";
+import { IconButton, Paper } from "@mui/material";
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component="form"
+      sx={{ borderRadius: 20, border: "1px solid #e3e3e3", boxShadow: "none", padding: "0 10px" }}
+    >
+      <input
+        placeholder="Search..."
+        style={{ border: "none", width: "350px", outline: "none" }}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
